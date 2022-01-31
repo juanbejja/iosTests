2892eb49cd00a963a29706b85c2d011adbf6c2d9
juanbejja <juanbejja@gmail.com>
2022-01-31 18:11:09 +0100

    add

---
+++ src/main/java/appium_ios/Base.java
@@ -0,0 +1,26 @@
+package appium_ios;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.remote.AutomationName;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class Base {
+
+	private static final String app = System.getProperty("user.home") + "/Desktop/UIKitCatalog.app";
+
+	public IOSDriver returnDriver() throws MalformedURLException {
+		DesiredCapabilities cap = new DesiredCapabilities();
+
+		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "15.2");
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 8");
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.IOS_XCUI_TEST);
+		cap.setCapability(MobileCapabilityType.APP, app);
+		IOSDriver driver = new IOSDriver<>(new URL("http://localhost:4723/wd/hub"), cap);
+		return driver;
+	}
+}
