87bcdacd1f58841a53b58fe00f88621cf4ba92b8
juanbejja <juanbejja@gmail.com>
2022-02-02 13:09:41 +0100

    delete variables unused

--- src/main/java/appium_ios/Base.java
+++ src/main/java/appium_ios/Base.java
@@ -11,32 +11,16 @@ import io.appium.java_client.remote.MobileCapabilityType;
 
 public class Base {
 
-	private static final String app = System.getProperty("user.home") + "/Desktop/UIKitCatalog2.app";
-	private static final String appw = "/Users/juan.pablo.bejarano/eclipse-workspace/appiumIOSBitrase/src/main/resources/UIKitCatalog.app";
 	private static final String app_bitrise = "/Users/vagrant/git/appium_automation/src/main/resources/UIKitCatalog.app";
 
 	public IOSDriver returnDriver() throws MalformedURLException {
-		System.out.println("---------------------");
-		System.out.println("---------------------");
-		System.out.println("---------------------");
-		System.out.println("---------------------");
-		System.out.println("---------------------");
-		System.out.println("APP PATH: " + System.getenv("BITRISE_APP_DIR_PATH"));
-		System.out.println("APP PATH: " + System.getenv("BITRISE_APP_DIR_PATH_LIST"));
-		System.out.println("---------------------");
-		System.out.println("---------------------");
-		System.out.println("---------------------");
-		System.out.println("---------------------");
-		System.out.println("---------------------");
 		DesiredCapabilities cap = new DesiredCapabilities();
-		
 
 		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "15.0");
 		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 8 Plus");
 		cap.setCapability(MobileCapabilityType.UDID, "0CC2A307-1FA2-48CF-8DB0-8216DC84F6C3");
 		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.IOS_XCUI_TEST);
 		cap.setCapability(MobileCapabilityType.APP, app_bitrise);
-//		cap.setCapability(MobileCapabilityType.APP, app_bitrise);
 
 		IOSDriver driver = new IOSDriver<>(new URL("http://localhost:4723/wd/hub"), cap);
 		return driver;
--- src/main/java/appium_ios/Test1.java
+++ src/main/java/appium_ios/Test1.java
@@ -23,19 +23,18 @@ public class Test1 extends Base {
 		scrollObject.put("direction", "down");
 		scrollObject.put("name", "Web View");
 
-		try {
-			System.out.println("ele: " + driver.findElement(By.xpath("//*")));
-		} catch (Exception e) {
-
-		}
 		driver.executeScript("mobile:scroll", scrollObject);
+		System.out.println("Scroll realizado correctamente");
 		driver.findElementByAccessibilityId("Web View").click();
+		System.out.println("Click en web view correcto");
 		driver.findElement(By.xpath("//XCUIElementTypeButton[@name='UIKitCatalog']")).click();
+		System.out.println("Click en UIKitCatalog correcto");
 		// picker
 		driver.findElementByAccessibilityId("Picker View").click();
 		driver.findElementByAccessibilityId("Red color component value").sendKeys("75");
 		driver.findElementByAccessibilityId("Green color component value").sendKeys("230");
 		driver.findElementByAccessibilityId("Blue color component value").sendKeys("115");
+		System.out.println("se setearon los colores Correctamente");
 
 	}
 
