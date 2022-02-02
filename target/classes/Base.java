b7ef8b71193ad549649940ad0113793e4f74f399
juanbejja <juanbejja@gmail.com>
2022-02-02 10:52:08 +0100

    bit

--- target/classes/Base.java
+++ target/classes/Base.java
@@ -12,6 +12,8 @@ import io.appium.java_client.remote.MobileCapabilityType;
 public class Base {
 
 	private static final String app = System.getProperty("user.home") + "/Desktop/UIKitCatalog.app";
+	private static final String appw = "/Users/juan.pablo.bejarano/eclipse-workspace/appiumIOSBitrase/src/main/resources/UIKitCatalog.app";
+	private static final String app_bitrise = "/appium_automation/src/main/resources/UIKitCatalog.app";
 
 	public IOSDriver returnDriver() throws MalformedURLException {
 		DesiredCapabilities cap = new DesiredCapabilities();
@@ -19,21 +21,14 @@ public class Base {
 		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "15.0");
 		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 8 Plus");
 		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.IOS_XCUI_TEST);
-//		cap.setCapability(MobileCapabilityType.APP, System.getenv("BITRISE_APK_PATH"));
-//		cap.setCapability(MobileCapabilityType.APP, app);
+//		cap.setCapability(MobileCapabilityType.APP, System.getenv("BITRISE_IPA_PATH"));
+		cap.setCapability(MobileCapabilityType.APP, app_bitrise);
 		System.out.println("---------------------");
 		System.out.println("---------------------");
 		System.out.println("---------------------");
 		System.out.println("---------------------");
 		System.out.println("---------------------");
-		System.out.println("---------------------");
-		System.out.println("---------------------");
-		System.out.println("APP PATH: "+System.getenv("BITRISE_APK_PATH"));
-		System.out.println("---------------------");
-		System.out.println("---------------------");
-		System.out.println("---------------------");
-		System.out.println("---------------------");
-		System.out.println("---------------------");
+		System.out.println("APP PATH: " + System.getenv("BITRISE_APK_PATH"));
 		System.out.println("---------------------");
 		System.out.println("---------------------");
 		System.out.println("---------------------");
